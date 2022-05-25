85fefeefbb0602952cf47617cb3bfd98f6fcda23
Olivier Michaelis <38879457+oliviermichaelis@users.noreply.github.com>
2022-05-25 23:58:47 +0200

    feat: implemented latency delay
    Signed-off-by: Olivier Michaelis <38879457+oliviermichaelis@users.noreply.github.com>

--- pkg/api/latency.go
+++ pkg/api/latency.go
@@ -3,9 +3,11 @@ package api
 import (
 	"github.com/stefanprodan/podinfo/pkg/fscache"
 	"go.uber.org/zap"
+	"math"
 	"math/rand"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -16,6 +18,12 @@ type LatencyMiddleware struct {
 
 func (l *LatencyMiddleware) Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		i := rand.Intn(100)
+		if i < 90 { // 90 percent are not affected
+			next.ServeHTTP(w, r)
+			return
+		}
+
 		v, ok := l.watcher.Cache.Load("latency")
 		if !ok {
 			l.logger.Error("unable to fetch /latency/latency file")
@@ -28,14 +36,14 @@ func (l *LatencyMiddleware) Handler(next http.Handler) http.Handler {
 			return
 		}
 
-		latency, err := strconv.Atoi(latencyString)
+		latency, err := strconv.Atoi(strings.TrimSpace(latencyString))
 		if err != nil {
 			l.logger.Error("failed to convert latency to int")
 			return
 		}
 
-		jitterRange := float64(latency) * 0.2 // 20 percent of jitter
-		jitter := rand.Intn(int(jitterRange))
+		jitterRange := float64(latency) * 0.2 // 20 percent of positive jitter
+		jitter := rand.Intn(int(math.Ceil(jitterRange)))
 
 		time.Sleep(time.Duration(latency+jitter) * time.Millisecond)
 		next.ServeHTTP(w, r)
@@ -49,6 +57,7 @@ func NewLatencyMiddleware(logger *zap.Logger) (*LatencyMiddleware, error) {
 	}
 
 	w.Watch()
+	logger.Info("started to watch /latency")
 	rand.Seed(time.Now().Unix())
 
 	return &LatencyMiddleware{
